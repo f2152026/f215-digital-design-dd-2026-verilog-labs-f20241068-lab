92efd8811d29898253db40547b8960a98d19bf63
f20241068-lab <f20241068@pilani.bits-pilani.ac.in>
2026-09-20 17:04:08 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -3,25 +3,48 @@
 
 module tb;
 
-  // TODO: declare the inputs and outputs
+    // Inputs
+    reg [1:0] t_sel;
 
-  // TODO: instantiate DUT here
+    // Output
+    wire [7:0] t_dout;
 
-  // Waveform dump configuration (DO NOT CHANGE)
-  string vcd_file;
-  initial begin
-    if ($value$plusargs("vcd=%s", vcd_file)) begin
-      $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+    // Instantiate DUT
+    lut #(
+        .WIDTH(8),
+        .DEPTH(4)
+    ) DUT (
+        .sel(t_sel),
+        .dout(t_dout)
+    );
+
+    // Waveform dump configuration (DO NOT CHANGE)
+    string vcd_file;
+    initial begin
+        if ($value$plusargs("vcd=%s", vcd_file)) begin
+            $dumpfile(vcd_file);
+            $dumpvars(0, DUT);
+        end
     end
-  end
 
-  initial begin
-    // TODO: apply different input combinations
+    // Apply different input combinations
+    initial begin
+        t_sel = 2'b00;
+        #5;
 
-  end
+        t_sel = 2'b01;
+        #5;
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+        t_sel = 2'b10;
+        #5;
+
+        t_sel = 2'b11;
+        #5;
+
+        $finish;
+    end
+
+    initial
+        $monitor($time, " sel=%b | dout=%b", t_sel, t_dout);
 
 endmodule
